175ad4ec5d310de72da9f8dc0888f5a71992e129
Carlos Santamaria <alexrace18@gmail.com>
2025-02-16 15:57:55 -0600

    Se hicieron ajustes

--- Portafolio/Repository/ProjectsProviderEnglishRepository.cs
+++ Portafolio/Repository/ProjectsProviderEnglishRepository.cs
@@ -7,13 +7,17 @@ namespace Portafolio.Provider
     {
         public List<Project> GetProjects()
         {
+
+            string commonTechnologies = "HTML, CSS, Bootstrap, JavaScript, jQuery, C#, ASP.NET, SQL";
+
+
             return new List<Project>
             {
                 new Project
                 {
                     Name = "Task Manager",
                     Description = "System that allows users to manage tasks, attach files, and register detailed steps.",
-                    Technologies = "HTML, CSS, BOOSTRAP, ASP.NET, JQUERY, KNOCKOUT.js",
+                    Technologies = $"{commonTechnologies}, Knockout.js",
                     ImageUrl = "/Images/TaskManagerImage.png",
                     GithubLink = "https://github.com/Charly7017/ManejoTareas",
                     Category = ProjectCategory.Web
@@ -22,19 +26,28 @@ namespace Portafolio.Provider
                 {
                     Name = "Accounts Payable System",
                     Description = "Provides detailed control of received invoices, pending payments, and due dates.",
-                    Technologies = "HTML, CSS, BOOTSTRAP, ASP.NET, JQUERY",
+                    Technologies = commonTechnologies,
                     ImageUrl = "/Images/accountspayablesystemImage.png",
                     GithubLink = "https://github.com/Charly7017/CuentasPorPagar",
                     Category = ProjectCategory.Web
                 },
                 new Project
                 {
-                    Name = "Super Hero App",
-                    Description = "Mobile application to manage heroes.",
-                    Technologies = "Xamarin, .NET",
-                    ImageUrl = "",
-                    GithubLink = "https://github.com/Charly7017/SuperHeroApp",
+                    Name = "iContainer",
+                    Description = "As a trainee at TAMSA, I participated in the development of this mobile application.",
+                    Technologies = "C#, .NET MAUI",
+                    ImageUrl = "/Images/iContainerImage.png",
+                    GithubLink = "https://play.google.com/store/apps/details?id=com.tenaris.iContainer&hl=es_419",
                     Category = ProjectCategory.Mobile
+                },
+                new Project
+                {
+                    Name = "Booking System",
+                    Description = "Web application for managing villa bookings with features like availability search, villa management, amenities tracking, and booking records.",
+                    Technologies = commonTechnologies,
+                    ImageUrl = "/Images/BookingSystemImage.png",
+                    GithubLink = "https://github.com/Charly7017/BookingSystem",
+                    Category = ProjectCategory.Web
                 }
             };
         }
--- Portafolio/Repository/ProjectsProviderSpanishRepository.cs
+++ Portafolio/Repository/ProjectsProviderSpanishRepository.cs
@@ -7,34 +7,46 @@ namespace Portafolio.Provider
     {
         public List<Project> GetProjects()
         {
+            string commonTechnologies = "HTML, CSS, Bootstrap, JavaScript, jQuery, C#, ASP.NET, SQL";
+
+
             return new List<Project>
             {
                 new Project
                 {
                     Name = "Manejador de tareas",
                     Description = "Sistema que permite a los usuarios gestionar tareas, anexar archivos y registrar pasos detallados.",
-                    Technologies = "HTML, CSS, BOOSTRAP, APS.NET, JQUERY, KNOCKOUT.js",
+                    Technologies = $"{commonTechnologies}, Knockout.js",
                     ImageUrl = "/Images/TaskManagerImage.png",
                     GithubLink = "https://github.com/Charly7017/ManejoTareas",
                     Category = ProjectCategory.Web
                 },
                 new Project
                 {
-                    Name = "Sistema de cuentas por pagar",
+                    Name = "Seguimiento de pagos",
                     Description = "Permite un control detallado de las facturas recibidas, los pagos pendientes y las fechas de vencimiento.",
-                    Technologies = "HTML, CSS, BOOTSTRAP, ASP.NET, JQUERY",
+                    Technologies = commonTechnologies,
                     ImageUrl = "/Images/accountspayablesystemImage.png",
                     GithubLink = "https://github.com/Charly7017/CuentasPorPagar",
                     Category = ProjectCategory.Web
                 },
                 new Project
                 {
-                    Name = "Super Hero App",
-                    Description = "Aplicación móvil para gestionar héroes.",
-                    Technologies = "Xamarin, .NET",
-                    ImageUrl = "",
-                    GithubLink = "https://github.com/Charly7017/SuperHeroApp",
+                    Name = "iContainer",
+                    Description = "Como prácticante en TAMSA, participe en el desarrollo de esta aplicación móvil.",
+                    Technologies = "C#, .NET MAUI",
+                    ImageUrl = "/Images/iContainerImage.png",
+                    GithubLink = "https://play.google.com/store/apps/details?id=com.tenaris.iContainer&hl=es_419",
                     Category = ProjectCategory.Mobile
+                },
+                new Project
+                {
+                    Name = "Booking System",
+                    Description = "Aplicación web para la gestión de reservas de villas, que permite buscar disponibilidad, administrar villas, gestionar servicios y consultar las reservas realizadas.",
+                    Technologies = commonTechnologies,
+                    ImageUrl = "/Images/BookingSystemImage.png",
+                    GithubLink = "https://github.com/Charly7017/BookingSystem",
+                    Category = ProjectCategory.Web
                 }
             };
         }
